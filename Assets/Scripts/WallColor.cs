c2b8c1b71d4a980a3ca97cbccc198e3ffd39de18
Ivan Andriukha <64382696+JackGhoster@users.noreply.github.com>
2023-05-13 01:58:02 +0300

    Gameplay - Walls
    Added walls that change their hue over time! Looks really pretty in my opinion :rainbow:

---
+++ Assets/Scripts/WallColor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallColor : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<SpriteRenderer> _walls = new List<SpriteRenderer>();
+
+    private float _hueMod = 0.1f;
+
+    private void Start()
+    {
+        InvokeRepeating("ChangeHueOverTime",0.2f,0.2f);
+    }
+
+    private void ChangeHueOverTime()
+    {
+        if (_hueMod <= 1f)
+        {
+            foreach (var sprite in _walls)
+            {
+                sprite.color = Color.HSVToRGB(_hueMod, 1f, 1f);
+            }
+            
+        }
+        else
+        {
+            _hueMod = 0f;
+        }
+        _hueMod += 0.01f;
+    }
+}
